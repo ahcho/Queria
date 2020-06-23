2637ccde629f9f056bf201f14df4b850c19b7004
Anna Cho <hbnch310@gmail.com>
2020-06-22 22:43:49 -0700

    Added firstname, lastname to user model

--- db/migrate/20200623004148_create_users.rb
+++ db/migrate/20200623004148_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2020_06_23_004148) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.datetime "created_at", null: false
