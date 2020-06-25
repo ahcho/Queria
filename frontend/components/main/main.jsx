3da4f802b730269a6bb453d968326b0345fae74a
Anna Cho <hbnch310@gmail.com>
2020-06-25 14:28:08 -0700

    Before updating CSS size

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class MainPage extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.user = this.props.currentUser;
+    }
+
     render() {
         return (
-            <div>
+            <div className='main-page'>
                 <p>this will be the main page in main.jsx</p>
                 <br/>
                 <br/>
                 <br/>  
-                <p>hello!!!! {this.props.currentUser.email}!!!!</p>
+                <p>{" " +this.user.first_name + " " + this.user.last_name} is logged in</p>
             </div>
         )
     }
