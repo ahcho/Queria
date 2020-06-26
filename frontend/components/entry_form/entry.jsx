73ea47cd7aae98ac8062a038f73c4ad14a5e15c5
Anna Cho <hbnch310@gmail.com>
2020-06-25 20:41:07 -0700

    Updated CSS

--- frontend/components/entry_form/entry.jsx
+++ frontend/components/entry_form/entry.jsx
@@ -53,38 +53,38 @@ class Entry extends React.Component {
         return (
             <main id="entry-page">
                 <div className="center-box">
-                    <h1 id="main-logo">Queria</h1>
-                    <br/>
-                    <h2 className="entry-tag-line">A place to share knowledge and better understand Willy Wonka's Chocolate Factory</h2>
-                    <div className="login-sigin-form">
-                        <form className="login">
-                            <h2 className="login-msg">Login</h2>
-                            <div className="user-input">
-                                <div id="login-email">
-                                    <input type="text" placeholder="Email" onChange={this.update("email")} value={this.state.email}/>
-                                </div>
-                                <div id="login-pw">
-                                    <input type="password" placeholder="password" onChange={this.update("password")}/>
-                                </div>
-                            </div>
-                            {this.renderError()}
-                            <div className="submit-button">
-                                <button type="submit" onClick={this.handleSubmit} >Login</button>
-                            </div>
-                        </form>
-                        
-                        <div className="signin">
-                            <button id='google-btn'>Anna's GitHub</button>
-                            <br/>
-                            <button id='fb-btn' type="submit" onClick={this.demoSubmit}>Demo</button>
-                            <br/>
-                            <button className='signup-form-open'id='queria-btn' onClick={() => this.props.openModal('signup')}>Sign Up With Email</button>
+                    <div className="entry-page-top">
+                        <div>
+                            <h1 id="main-logo">Queria</h1>
+                            <h2 className="entry-tag-line">A place to share knowledge and better understand Willy Wonka's Chocolate Factory</h2>
                         </div>
-                        
-                        <div className="vertical"></div> 
-
-
-                        
+                    </div>
+                    <div className="entry-page-bottom">
+                        <div className=" login-signup-form">
+                            <form className="login">
+                                <h2 className="login-msg">Login</h2>
+                                <div className="user-input">
+                                    <div id="login-email">
+                                        <input type="text" placeholder="Email" onChange={this.update("email")} value={this.state.email}/>
+                                    </div>
+                                    <div id="login-pw">
+                                        <input type="password" placeholder="password" onChange={this.update("password")}/>
+                                    </div>
+                                </div>
+                                {this.renderError()}
+                                <div className="submit-button">
+                                    <button type="submit" onClick={this.handleSubmit} >Login</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="signup-2">
+                            <div className="signup">
+                                <button id='google-btn'>Anna's GitHub</button>
+                                <button id='fb-btn' type="submit" onClick={this.demoSubmit}>Demo</button>
+                                <button className='signup-form-open'id='queria-btn' onClick={() => this.props.openModal('signup')}>Sign Up With Email</button>
+                            </div>
+                            {/* <div className="vertical"></div>                          */}
+                        </div>
                     </div>
                 </div>
             </main>
