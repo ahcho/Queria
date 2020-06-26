5fac2315d21a93b7663e108b07c19d0137a86a76
Anna Cho <hbnch310@gmail.com>
2020-06-26 00:09:33 -0700

    Fixed sign up

--- frontend/components/signup_form/signup.jsx
+++ frontend/components/signup_form/signup.jsx
@@ -38,38 +38,37 @@ class SignUpForm extends React.Component {
 
         return (
             <div className="signup-form">
-                <h1 id='signup-msg'>Sign up with Queria!</h1>
+                <h1 id='signup-msg'>Join Queria!</h1>
                 <br/>
-                <form onSubmit={this.handleSubmit}>   
-                    <div id="user-name">
-                        <label>FIRST NAME
-                            <br/>
-                            <input type="text" onChange={this.update("fname")} value={this.state.fname} />
-                        </label>
-                        <br/>
-                        <br/>
-                        <label>LAST NAME
-                            <br/>
-                            <input type="text" onChange={this.update("lname")} value={this.state.lname} />
-                        </label>
-                    </div>
-                    <br />
+                <form onSubmit={this.handleSubmit}>  
+                    <div class="signup-input">
+                        <div id="user-fname">
+                            <label>First name
+                                <br />
+                                <input type="text" placeholder="First name" onChange={this.update("fname")} value={this.state.fname} />
+                            </label>
+                        </div>
+                        <div id="user-lname">
+                            <label>Last name
+                                <br />
+                                <input type="text" placeholder="Last name" onChange={this.update("lname")} value={this.state.lname} />
+                            </label>
+                        </div>
 
-                    <label>EMAIL
-                        <br />
-                        <input type="text" onChange={this.update("signup_email")} value={this.state.signup_email} />
-                    </label>
-                    <br/>
-                    <br/>
-                    <label>PASSWORD
-                        <br />
-                        <input type="password" onChange={this.update("signup_pw")} value={this.state.signup_pw} />
-                    </label>
-                    <br />
-                    <div className="signup-btn">
-                        <button type="submit" onClick={() => this.props.closeModal()}>Cancel</button> 
-                        <button type="submit" onClick={this.handleSubmit}>Sign Up</button>        
-                    </div>
+                        <div id="signup-email">Email
+                            <br />
+                            <input type="text" placeholder="Email" onChange={this.update("signup_email")} value={this.state.signup_email} />
+                        </div>
+                
+                        <div id="signup-password">Password(6 or more characters)
+                            <br />
+                            <input type="password" placeholder="Password(6 or more characters)" onChange={this.update("signup_pw")} value={this.state.signup_pw} />
+                        </div>
+                        <div className="signup-btn">
+                            <button type="submit" onClick={() => this.props.closeModal()}>Cancel</button> 
+                            <button type="submit" onClick={this.handleSubmit}>Sign Up</button>        
+                        </div>
+                    </div >
                     <div id={`signin-error ${this.props.errors ? "" : "no-error"}`}>
                         {this.renderErrors()}
                     </div>
