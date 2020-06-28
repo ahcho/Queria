08becf8712093ac18b0756866acbbf08596c4160
Anna Cho <hbnch310@gmail.com>
2020-06-28 08:37:53 -0700

    Added routes for answers

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:create]
+    resource :user, only: [:create] do
+      resources :answers, only: [:show]
+    end
     resource :session, only: [:create, :destroy, :show]
-    resources :questions, only: [:index, :create, :show, :update, :destroy]
-    resources :answers, only: [:index, :create, :show, :update, :destroy]
+    resources :questions, only: [:index, :create, :show, :update, :destroy] do
+      resources :answers, only: [:index]
+    end
+    resources :answers, only: [:create, :update, :destroy]
   end
 
 end
---
+++ frontend/util/answer_api_util.js
@@ -0,0 +1,43 @@
+export const fetchAnswers = (questionId) => {
+    return $.ajax({
+        method: "GET",
+        url: `/api/questions/${questionId}/answers`
+    })
+}
+///api/questions/:question_id/answers(.:format)   
+//call to get answer by a logged in user
+export const fetchAllUserAnswers = (userId) => {
+    return $.ajax({
+        method: "GET",
+        url: `api/user/answers/${userId}`
+    })
+}
+
+export const createAnswer = (answer) => {
+    return $.ajax({
+        method: "POST",
+        url:   `/api/answers`,
+        data: { answer }
+    })
+}
+
+export const updateAnswer = (answer) => {
+    return $.ajax({
+        method: "PATCH",
+        url: `/api/answers/${answer.id}`,
+        data: { answer }
+    })
+}
+
+export const deleteAnswer = (answerId) => {
+    return $.ajax({
+        method: "DELETE",
+        url: `/api/answers/${answerId}`
+    })
+}
+// export const fetchQuestion = (id) => {
+//     return $.ajax({
+//         method: "GET",
+//         url: `/api/questions/${id}`
+//     })
+// };
