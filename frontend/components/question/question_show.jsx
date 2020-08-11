66042b32e9fc0a04d4020028e3cba291a3c89a79
Anna Cho <hbnch310@gmail.com>
2020-08-10 19:26:49 -0700

    Updated photo rendering on question show page

--- frontend/components/question/question_show.jsx
+++ frontend/components/question/question_show.jsx
@@ -19,19 +19,31 @@ class QuestionShow extends React.Component {
         this.props.fetchQuestion(this.props.match.params.questionId);
     }
 
+    questionPhotoAttached() {
+        // debugger
+        if (this.props.question.questionPhotoUrl) {
+            // debugger
+            return (
+                <img src={this.props.question.questionPhotoUrl} src="" />
+            )
+        } else {
+            return (
+                <img src="https://images.unsplash.com/photo-1567206563064-6f60f40a2b57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"/>
+            )
+        }
+    }
 
     render() {
         const { currentUser, question } = this.props;
         const dropdown = this.state.dropDown ? "" : "hidden" ;
         if (!question ) return null;
-    
         return (
             <div>
                 <div className='q-show-header'>
-                    {/* <img src={ url_for(question.photo)} alt=""/> */}
-                    <h1>{question.question}</h1>
                     <p>{question.author.first_name} asked</p>
                     <p>{question.created_at.slice(0, 10)} at {question.created_at.slice(11, 16)}</p>
+                    <h1>{question.question}</h1>
+                    {this.questionPhotoAttached()}
                 </div>
                 <div className='q-answer-box'>
                     <p>{this.props.currentUser.first_name}, can you answer this question?</p>
